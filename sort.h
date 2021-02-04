da6dbcf56aef59a2afc987878f5e5780d1c2c796
Peter5793 <masaba.peter68@gmail.com>
2021-02-04 19:31:30 +0300

    Implement quick sort method

--- sort.h
+++ sort.h
@@ -22,6 +22,13 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void q_sort(int *array, int lo, int hi, size_t size);
+void swap_vals(int *ai, int *aj);
+void shell_sort(int *array, size_t size);
 
 #endif
